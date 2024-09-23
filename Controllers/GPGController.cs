69dee3d7c4a9fd912e42ef19964c2206df44d7c4
Benjamin Belmar <benjamin.belmar@usach.cl>
2024-09-23 16:41:15 -0300

    feat: first version, encrypt and decrypt methods

---
+++ Controllers/GPGController.cs
@@ -0,0 +1,43 @@
+﻿namespace GPGTest.Controllers;
+
+using GPGTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class GpgController : ControllerBase
+{
+    private readonly GpgService _gpgService;
+
+    public GpgController()
+    {
+        // Pass the paths to the public and private keys here
+        _gpgService = new GpgService("Keys/public.asc", "Keys/private.asc");
+    }
+
+    [HttpPost("encrypt")]
+    public IActionResult Encrypt([FromBody] EncryptRequest data)
+    {
+        var encryptedText = _gpgService.Encrypt(data.PlainText);
+        return Ok(encryptedText);
+    }
+
+    [HttpPost("decrypt")]
+    public IActionResult Decrypt([FromBody] DecryptRequest request)
+    {
+        var decryptedText = _gpgService.Decrypt(request.EncryptedText, request.Passphrase);
+        return Ok(decryptedText);
+    }
+}
+public class EncryptRequest
+{
+    public string PlainText { get; set; } = "";
+}
+
+public class DecryptRequest
+{
+    public string EncryptedText { get; set; } = "";
+    public string Passphrase { get; set; } = "";
+}
---
+++ Services/GPGService.cs
@@ -0,0 +1,93 @@
+﻿namespace GPGTest.Services;
+
+using Org.BouncyCastle.Bcpg;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Security;
+using System.IO;
+using System.Text;
+public class GpgService
+{
+    private readonly string _publicKeyPath;
+    private readonly string _privateKeyPath;
+
+    public GpgService(string publicKeyPath, string privateKeyPath)
+    {
+        _publicKeyPath = publicKeyPath;
+        _privateKeyPath = privateKeyPath;
+    }
+
+    // Encrypts the input string using the public key
+    public string Encrypt(string input)
+    {
+        using (var publicKeyStream = File.OpenRead(_publicKeyPath))
+        using (var outputStream = new MemoryStream())
+        {
+            var clearData = Encoding.UTF8.GetBytes(input); // Convert input to UTF-8 bytes
+            PgpPublicKey pubKey = ReadPublicKey(publicKeyStream);
+
+            var encryptedDataGenerator = new PgpEncryptedDataGenerator(SymmetricKeyAlgorithmTag.Aes256, true, new SecureRandom());
+            encryptedDataGenerator.AddMethod(pubKey);
+
+            using (var armoredOutputStream = new ArmoredOutputStream(outputStream))
+            using (var encryptionStream = encryptedDataGenerator.Open(armoredOutputStream, clearData.Length))
+            {
+                encryptionStream.Write(clearData, 0, clearData.Length);
+            }
+
+            return Convert.ToBase64String(outputStream.ToArray()); // Return base64 encoded string
+        }
+    }
+
+    // Decrypts the encrypted text using the private key and passphrase
+    public string Decrypt(string encryptedText, string passphrase)
+    {
+        using (var privateKeyStream = File.OpenRead(_privateKeyPath))
+        using (var inputStream = new MemoryStream(Convert.FromBase64String(encryptedText)))
+        {
+            PgpObjectFactory pgpF = new PgpObjectFactory(PgpUtilities.GetDecoderStream(inputStream));
+            PgpEncryptedDataList enc = (PgpEncryptedDataList)(pgpF.NextPgpObject() ?? pgpF.NextPgpObject());
+
+            PgpSecretKeyRingBundle pgpSec = new PgpSecretKeyRingBundle(PgpUtilities.GetDecoderStream(privateKeyStream));
+
+            foreach (PgpPublicKeyEncryptedData pked in enc.GetEncryptedDataObjects())
+            {
+                PgpPrivateKey privateKey = FindSecretKey(pgpSec, pked.KeyId, passphrase.ToCharArray());
+                if (privateKey != null)
+                {
+                    using (var clearStream = pked.GetDataStream(privateKey))
+                    using (var reader = new StreamReader(clearStream))
+                    {
+                        return reader.ReadToEnd(); // Return decrypted text
+                    }
+                }
+            }
+        }
+
+        throw new ArgumentException("Secret key for decryption not found.");
+    }
+
+    // Reads the public key from the stream
+    private PgpPublicKey ReadPublicKey(Stream input)
+    {
+        PgpPublicKeyRingBundle pgpPub = new PgpPublicKeyRingBundle(PgpUtilities.GetDecoderStream(input));
+        foreach (PgpPublicKeyRing keyRing in pgpPub.GetKeyRings())
+        {
+            foreach (PgpPublicKey key in keyRing.GetPublicKeys())
+            {
+                if (key.IsEncryptionKey)
+                {
+                    return key;
+                }
+            }
+        }
+        throw new ArgumentException("Encryption key not found in the public key ring.");
+    }
+
+    // Finds the secret (private) key from the key ring bundle using the key ID and passphrase
+    private PgpPrivateKey FindSecretKey(PgpSecretKeyRingBundle pgpSec, long keyId, char[] passphrase)
+    {
+        PgpSecretKey secretKey = pgpSec.GetSecretKey(keyId);
+        return secretKey?.ExtractPrivateKey(passphrase);
+    }
+}
+
